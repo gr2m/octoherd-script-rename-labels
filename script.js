74b18a981c35de9d19a9224a4c6235bdface59f8
Gregor Martynus <39992+gr2m@users.noreply.github.com>
2021-12-21 14:17:56 -0800

    feat: initial version

--- script.js
+++ script.js
@@ -9,4 +9,29 @@
  * @param {string} options.find string to replace in existing label names
  * @param {string} options.replace replacement for the find string in existing labels
  */
-export async function script(octokit, repository, { find, replace }) {}
+export async function script(octokit, repository, { find, replace }) {
+  if (!find || !replace) {
+    octokit.log.error("Both --find and --replace are required");
+    process.exit(1);
+  }
+  for await (const { data: labels } of octokit.paginate.iterator(
+    "GET /repos/{owner}/{repo}/labels",
+    {
+      owner: repository.owner.login,
+      repo: repository.name,
+    }
+  )) {
+    const labelNames = labels.map((label) => label.name);
+    octokit.log.info(`Labels found: ${labelNames.join(", ")}`);
+    for (const label of labels) {
+      if (!label.name.includes(find)) continue;
+
+      await octokit.request("PATCH /repos/{owner}/{repo}/labels/{name}", {
+        owner: repository.owner.login,
+        repo: repository.name,
+        name: label.name,
+        new_name: label.name.replace(find, replace),
+      });
+    }
+  }
+}
